c616692fd69e4d086e0591e4e514f8ebeaac7a48
Freezi <freeziftw@gmail.com>
2021-10-29 14:07:53 +0200

    Add bare bone server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::{io, env};
 use dotenv::dotenv;
+use actix_web::{App, HttpServer};
 
-fn main() {
+#[actix_web::main]
+async fn main() -> io::Result<()> {
     dotenv().ok();
+
+    let server_address = env::var("SERVER_ADDRESS")
+        .expect("No server address was provided.");
+
+    HttpServer::new(|| {
+        App::new()
+    })
+        .bind(server_address)?
+        .run()
+        .await
 }
