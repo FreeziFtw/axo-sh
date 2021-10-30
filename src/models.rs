3292e063553883cd0478edc1a3fd34c1da06d777
Freezi <freeziftw@gmail.com>
2021-10-30 13:34:20 +0200

    Update models

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,17 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Url {
-    pub url: url::Url,
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ShortUrl {
+    pub id: String,
+    pub url: String,
+}
+
+#[derive(Serialize)]
+pub struct Id {
+    pub id: String,
 }
